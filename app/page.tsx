e837eaf6230523bedceeba5a451cf8c2da02862a
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-05-25 02:23:12 -0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-2xl">Projeto Dashboard</h1>
+      <Button>Teste button</Button>
     </main>
   );
 }
